c7c6549f758cd17ddf901fb59f1263f52e835bb9
makooster <m_akhmadi@kbtu.kz>
2024-04-17 15:57:41 +0500

    lab10 started

---
+++ lab10/hh_back/api/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+class Company(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    city = models.CharField(max_length = 50)
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.name, self.description, self.city, self.address}"
+    
+    def to_json(self):
+        return {
+            "name" : self.name,
+            "description" : self.description,
+            "city" : self.city,
+            "address" : self.address
+        }
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField()
+    salary = models.FloatField()
+    company = models.ForeignKey(Company, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name, self.description, self.salary, self.company}"
+    
+    def to_json(self):
+        return {
+            "name" : self.name,
+            "description" : self.description,
+            "city" : self.salary,
+            "address" : self.company
+        }
+    class Meta:
+        verbose_name = "Vacancy"
+        verbose_name_plural = "Vacancies"
+
+
+
---
+++ lab10/hh_back/api/views.py
@@ -0,0 +1,96 @@
+from rest_framework.decorators import api_view
+# from rest_framework.decorators import csrf_exempt
+from rest_framework.response import Response
+from .serializers import CompanySerializer, VacancySerializer
+from .models import Company, Vacancy
+
+
+@api_view(['GET'])
+def companyList(request):
+    companies = Company.objects.all()
+    serializer = CompanySerializer(companies, many = True)
+    return Response(serializer.data)
+
+# @api_view(['GET'])
+# def companiesAndVacancies(request):
+#     companies = Company.objects.all()
+#     serializer = CompanySerializer(companies, many = True)
+#     return Response(serializer.data)
+
+@api_view(['GET'])
+def companyListDetail(request, id):
+    companies = Company.objects.get(pk = id)
+    serializer = CompanySerializer(companies, many = False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def vacancyList(request):
+    vacancies = Vacancy.objects.all()
+    serializer = VacancySerializer(vacancies, many = True)
+    return Response(serializer.data)
+
+# I replaced places of pk and id, if it won't work i should change it. and should also change urls file
+
+# mannnnn it is fine, that is the proper way to work
+
+@api_view(['GET'])
+def vacancyListDetail(request, id):
+    vacancies = Vacancy.objects.get(pk = id)
+    serializer = VacancySerializer(vacancies, many = False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def vacancyCreate(request):
+    serializer = VacancySerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def companyCreate(request):
+    serializer = CompanySerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def companyUpdate(request, id):
+    company = Company.objects.get(pk = id)
+    serializer = CompanySerializer(instance = company, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def vacancyUpdate(request, id):
+    vacancy = Vacancy.objects.get(pk = id)
+    serializer = VacancySerializer(instance = vacancy, data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def companyDelete(request, id):
+    company = Company.objects.get(pk = id)
+    company.delete()
+    return Response('Company successfully deleted!')
+
+@api_view(['DELETE'])
+def vacancyDelete(request, id):
+    vacancy = Vacancy.objects.get(pk = id)
+    vacancy.delete()
+    return Response('Vacancy succesfully deleted!')
+
+# class VacancyTopTen(APIView):
+#     def get(self,request):
+#         ten_top = Vacancy.objects.all().order_by('-salary')[:10]
+#         serializer = VacancySerializer(ten_top, many=True)
+#         return Response(serializer.data)
